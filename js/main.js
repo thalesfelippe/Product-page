55c884717ac0d793bcb330a4f00a8d48bd904ac6
thalesfelippe <72764998+thalesfelippe@users.noreply.github.com>
2021-04-15 10:02:31 -0300

    Products and testimonials making

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    //product carousel
+    $('#product-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+});
