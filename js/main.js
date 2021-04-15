4a658662d5176fd4375f6e8300c2be719749fb39
Your Name <thalesfelippe>
2021-04-15 16:43:20 -0300

    Faq and newsletter

--- js/main.js
+++ js/main.js
@@ -19,4 +19,25 @@ $(document).ready(function(){
             }
         }
     })
+
+    //testimonial
+    $('#testimonial-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
+            }
+        }
+    })
 });
